d5e84a0b36fda1ad714b9a46ef89b4ca3298819e
ajp02 <ajp02@asilla.net>
2020-08-23 00:03:21 +0900

    fix loi nang luong

--- Miner-Testing-CodeSample/build/heuristic_model1.py
+++ Miner-Testing-CodeSample/build/heuristic_model1.py
@@ -48,7 +48,7 @@ class Heuristic_1:
         # next_cell = self.path_to_des.pop()
         next_cell = self.path_to_des[1]
 
-        if -self.lost_energy_next_step(next_cell)  > self.state.energy:
+        if -self.lost_energy_next_step(next_cell)  >= self.state.energy:
             return 4
         
         action = self.convert_point_to_action(next_cell)
